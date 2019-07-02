069e0ecdb92dc66b7dd6706d9b53c3fb7ffe63fc
Chandradeep Dey <chandradeepdey@posteo.net>
2019-07-03 04:30:06 +0530

    Added Gaussian Elimination with backward substitution to linear systems

---
+++ Linear systems/Gaussian Elimination/main.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "functions.h"
+
+int main(int argc, char * argv[])
+{
+        int n;
+        double ** matrix;
+        double * solution;
+
+        int status = allocate(&matrix, &solution, &n);
+        if (status == 1)
+                exit(EXIT_FAILURE);
+        else if (status == 2) {
+                fputs("Quitting program due to invalid number of equations.\n",
+                stderr);
+                exit(EXIT_SUCCESS);
+        }
+
+        if (takeInput(matrix, n)) {
+                fputs("Quitting program due to EOF or read error.\n", stderr);
+                deallocate(matrix, solution, n);
+                exit(EXIT_FAILURE);
+        }
+        puts("Here is the augmented matrix:");
+        printMatrix(matrix, n);
+
+        if (triangularMatrix(matrix, n)) {
+                fputs("Cannot solve the system of linear equations.\n", stderr);
+                deallocate(matrix, solution, n);
+                exit(EXIT_FAILURE);
+        }
+        puts("Here is the matrix after elementary row operations:");
+        printMatrix(matrix, n);
+
+        solve(matrix, solution, n);
+        puts("Here is the solution vector:");
+        printSolution(solution, n);
+
+        deallocate(matrix, solution, n);
+        return EXIT_SUCCESS;
+}
